4ce5c61bce3fd74d24d1d27bc0a855d532508ad6
DylanNoally <dylan.window@hotmail.fr>
2017-03-29 16:41:43 +0200

    Delete formulaireDeConnexion.php

--- view/includes/formulaireDeConnexion.php
+++
@@ -1,9 +0,0 @@
-<form action="#" method="POST" >
-	<label>Entrez votre Pseudonyme</label> : <input type="text" name="pseudonyme" />
-	<br>
-	<label>Entrez votre mot de passe</label> : <input type="password" name="motdepasse" />
-	<br>
-	<br>
-	
-	<input type="submit" name="formulaireConnexion" value="Valider vos informations !">
-</form>

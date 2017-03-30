f55f0830816a6e44130e5999017945879be7002d
DylanNoally <dylan.window@hotmail.fr>
2017-03-30 13:01:20 +0200

    Add files via upload

---
+++ couleur.php
@@ -0,0 +1,25 @@
+<?php
+		/* On regarde à présent si le cookie existe déjà
+		 * (soit parce que l'internaute a émis une préférence,
+		 * soit simplement parce qu'il/elle est connu·e) */
+		if (key_exists('couleurDuTexte', $_COOKIE)) 
+		{
+			$couleur = $_COOKIE['couleurDuTexte'];
+		}
+		// Couleur par défaut, si l'utilisateur arrive pour la première fois sur le site //
+		else
+		{
+			$couleur = "black";			
+		}
+		
+		if (isset($_GET['boutoncouleur']) && isset($_GET['couleurtexte'])) 
+		{
+			setcookie('couleurDuTexte', $_GET['couleurtexte'], time() + 30);
+			$couleur = $_GET['couleurtexte'];
+		}
+?>
+
+<style type="text/css">
+	p { color: <?php echo $couleur; ?> }
+</style>
+

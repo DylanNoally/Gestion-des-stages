68e3f34a59ba05d87fdcae342fdb82756f4a1323
DylanP60 <popyk.dylan@9online.fr>
2017-04-21 13:01:25 +0200

    Add files via upload

--- anneesScolaires.php
+++ anneesScolaires.php
@@ -1,6 +1,9 @@
 	<?php
 		session_start();
 		include 'view/includes/header.php';
+    	include 'view/includes/connexionBD.php';
+    	include 'view/includes/traitementAnneesScolaires.php';
+    	include 'view/includes/traitementAnneesScolairesSuppr.php';    	
 	?>
 
 	<h1>Suivi scolarité</h1>
@@ -9,6 +12,14 @@
 			<div class="left1">
 				<h3>Liste des années scolaires</h3>
 				<br>
+					<?php 
+					          
+					$query = $bdd->prepare('SELECT Annee, Id_date_annee FROM annee');
+					          
+					$query->execute();
+					$results = $query->fetchAll();
+					?>
+		
 				<table border class="table23">
 			   		<thead> <!-- En-tête du tableau -->
 			       		<tr>
@@ -16,28 +27,38 @@
 			           		<th>Actions</th>
 			       		</tr>
 			   		</thead>
+					<?php         
+					foreach ($results as $eleve) 
+					          
+					{
+					?>
 			   		<tbody>
 			   			<tr>
-			   				<td>2017/2018</td>
-			   				<td>Supprimer</td>
-			   			</tr>
-			   			<tr>
-			   				<td>2016/2017</td>
-			   				<td>Supprimer</td>
-			   			</tr>
-			   			<tr>
-			   				<td>2015/2016</td>
-			   				<td>Supprimer</td>
+			   				<td><option value="<?php echo $eleve['Id_date_annee']; ?>">
+								<?php echo $eleve['Annee']; ?>
+								</option>
+							</td>
+			   				<td>
+			   					<form method="post" action="anneesScolaires.php">
+			   					<input type="button" value="Supprimer" class="Suppr" name="Supprimer">
+			   					</form>	
+			   				</td>
 			   			</tr>		
 			   		</tbody>
+					<?php
+					}
+					?>	
 				</table>
 			</div>
 
 			<div class="right1">
 				<h3>Ajouter une année</h3>
 				<br>
-				Nom : <input type="textarea" class="textarea23" name="Nom">
+				<form method="post" action="anneesScolaires.php">				
+				Nom : <input type="text" class="text23" name="Nom">
 				<br> <br>
 				<input type="submit" class="Ajouter2" name="Ajouter">
+				</form>
 			</div>
-		</div>		
+		</div>	
+	
--- typesDeBAC.php
+++ typesDeBAC.php
@@ -1,6 +1,8 @@
 	<?php
 		session_start();
 		include 'view/includes/header.php';
+		include 'view/includes/connexionBD.php';
+		include 'view/includes/traitementTypesBAC.php';
 	?>
 
 	<h1>Suivi scolarité</h1>
@@ -9,6 +11,13 @@
 			<div class="left1">
 				<h3>Liste des BAC</h3>
 				<br>
+					<?php 
+					          
+					$query = $bdd->prepare('SELECT Libelle_type_bac, Id_type_bac FROM typebac');
+					          
+					$query->execute();
+					$results = $query->fetchAll();
+					?>				
 				<table border class="table24">
 			   		<thead> <!-- En-tête du tableau -->
 			       		<tr>
@@ -16,28 +25,34 @@
 			           		<th>Actions</th>
 			       		</tr>
 			   		</thead>
+					<?php         
+					foreach ($results as $eleve) 
+					          
+					{
+					?>			   		
 			   		<tbody>
 			   			<tr>
-			   				<td>BAC Général S</td>
-			   				<td>Supprimer</td>
-			   			</tr>
-			   			<tr>
-			   				<td>BAC Général ES</td>
-			   				<td>Supprimer</td>
-			   			</tr>
-			   			<tr>
-			   				<td>BAC PRO STII</td>
+			   				<td><option value="<?php echo $eleve['Id_type_bac']; ?>">
+								<?php echo $eleve['Libelle_type_bac']; ?>
+								</option>
+							</td>
 			   				<td>Supprimer</td>
 			   			</tr>		
 			   		</tbody>
+					<?php
+					}
+					?>				   		
 				</table>
 			</div>
 
 			<div class="right1">
 				<h3>Ajouter un nouveau BAC</h3>
 				<br>
-				Intitulé : <input type="textarea" class="textarea24" name="Intitulé">
+				<form method="post" action="typesDeBAC.php">					
+				Intitulé : <input type="text" class="text24" name="Intitulé">
 				<br> <br>
 				<input type="submit" class="Ajouter3" name="Ajouter">
+				</form>
 			</div>
 		</div>		
+

eb29bd98f420ebf042932c1342b753d392506247
DylanNoally <dylan.window@hotmail.fr>
2017-04-22 14:20:13 +0200

    Add files via upload

--- entreprises.php
+++ entreprises.php
@@ -11,17 +11,30 @@
 				<h2>Entreprises</h2>
 					<br>
 					
-					<h2>Rechercher une entreprise</h2>
-
-					<br>
+					<h3>Rechercher une entreprise :</h3>
 					<br>
 					<form action="informationsEntreprise.php" method="GET" >
 						<label>Nom de l'entreprise :</label>
 				        <select name="entrepriseExistante" id="entrepriseExistante">
-				           <option value="nodevo">Nodevo</option>
-				           <option value="mentalworks">MentalWorks</option>
-				        </select>
+				           <?php 
+		           			$query = $bdd->prepare('SELECT Id_entreprise, Nom_entreprise FROM entreprise');
+		           			$query->execute();
 
+		           			$resultatNomEntreprise = $query->fetchAll();
+							
+				        	foreach ($resultatNomEntreprise as $nomEntreprise) 
+		           			{
+							?>
+
+        					<option value="<?php echo $nomEntreprise['Id_entreprise']; ?>">
+    							<?php echo $nomEntreprise['Nom_entreprise']; ?>
+        					</option>
+    						<?php
+							}
+
+							?>
+				        </select>
+				        <br>
 				        <input type="submit" name="rechercheEntreprise" value="Rechercher">
 				    </form>
 			</div>
@@ -30,11 +43,12 @@
 				<h2>Nouvelle entreprise</h2>
 					<br>
 					<br>
-					<form action="informationsEntreprise.php" method="GET" >
+					<form action="informationsEntreprise.php" method="POST" >
 						Nom de l'entreprise :<input type="text" name="nouvelleEntreprise">
 						<br><br>
-						Chiffre d'affaires :<input type="text" name="nouvelleEntreprise">
+						Chiffre d'affaires :<input type="text" name="chiffreAffaires">
 						<br><br>
-						Adresse postale :<input type="text" name="nouvelleEntreprise">
+						Adresse postale :<input type="text" name="adressePostale">
 						<br><br>
 						<div align="center"><input type="submit" name="creerEntreprise" value="Valider"></div>
+					</form>

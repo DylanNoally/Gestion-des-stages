47782e9f54ace9fdfc14884f94643094c25467f2
GradiM <gradi.mandambu@outlook.com>
2017-04-23 20:20:51 +0200

    Add files via upload

--- view/includes/header.php
+++ view/includes/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="stylesheet" href="public/css/x.css">
+	<link rel="stylesheet" href="public/css/yx.css">
 	<link rel="stylesheet" href="public/css/naav.css" type="text/css" media="screen">
 	<link rel="stylesheet" href="public/css/stylesheet.css" type="text/css" charset="utf-8" />
 	<title>Acceuil</title>

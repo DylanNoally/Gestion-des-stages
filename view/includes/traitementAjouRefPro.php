a0cca57ba0891b07047cf09afd5ab75563a82023
GradiM <gradi.mandambu@outlook.com>
2017-04-27 00:42:57 +0200

    Add files via upload

--- view/includes/traitementAjouRefPro.php
+++ view/includes/traitementAjouRefPro.php
@@ -8,18 +8,30 @@
 		$query->bindParam(':fonctionRef' , $fonctionRef);
 
 		// Vérification de l'existence des valeurs du formulaire
-		if (isset($_POST["nom"])) {
-			if (isset($_POST["fonction"])) {
-				// Récuperation des valeurs du formulaire
-				$nomRef = $_POST["nom"];
-				$fonctionRef = $_POST["fonction"];
+		if (isset($_POST["nom"]) && isset($_POST["fonction"])) {
+			if(empty($_POST["nom"])) {
+					?>
+					<script type="text/javascript"> alert("Veuillez inscrire le nom du référent"); </script> 
+					<?php
+				}
 
-				// Exécution de la requête 
-				$query->execute();
-			}
-			
-		}		
+				elseif ($_POST["nom"] != 'string') {
+					?>
+					<script type="text/javascript"> alert("Un nom ne possède pas de chiffres ou de caractères spéciaux !"); </script> 
+					<?php
+				}
+		
+				else {
+					// Récuperation des valeurs du formulaire
+					$nomRef = $_POST["nom"];
+					$fonctionRef = $_POST["fonction"];
+
+					// Exécution de la requête 
+					$query->execute();
+				}
+		}
 	}
+					
 	elseif ($bdd == false) {
 		echo "Echec de connexion à la base de données";
 	}

b70e7bbe6252080b282aab18d9e2a569339c1b98
Dylan Noally <Dylan Noally>
2017-04-29 16:26:22 +0200

    Test avec Git Bash

--- stage.php
+++ stage.php
@@ -1,55 +1,75 @@
 <?php
-	// On démarre la session AVANT d'écrire du code HTML
-	session_start();
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
 
-	include 'view/includes/header.php';
+// On include le code permetant d'envoyer l'utilisateur dans la page d'acceuil s'il n'est pas connecté 
+//include 'view/includes/retourEnForce.php';
+
+// On inclue le menu et la connexion à la page
+include 'view/includes/header.php';
 ?>
 
 <div class="container">
 		<div id="content">
-			<div class="left">
-				<h2>Rechercher un élève</h2>
+			<div class="rechercheEleve">
+				<h1>Rechercher un élève</h1>
 				<br>
-				<form action="ensembleStages.php" method="GET" >
-					<label>Classe :</label>
-			        <select name="classe" id="classe">
-		        		<?php 
-		           			$query = $bdd->prepare('SELECT Nom_classe, Id_classe FROM classe');
-		           			$query->execute();
-		           			$results = $query->fetchAll();
+				<div class="formulaire">
+					<form action="ensembleStages.php" method="GET" >
+						<div class="annee">
+							<label>Classe :</label>
+							<div class="nomAnnee">
+						        <select name="classe" id="classe">
+					        		<?php 
+					           			$query = $bdd->prepare('SELECT Nom_classe, Id_classe FROM classe');
+					           			$query->execute();
+					           			$results = $query->fetchAll();
+					           			foreach ($results as $classe) 
+					           			{
+									?>
 
+			        					<option value="<?php echo $classe['Id_classe']; ?>">
+			    							<?php echo $classe['Nom_classe']; ?>
+			        					</option>
+			    					<?php
+										}
+									?>
+						        </select>
+					        </div>
+				        </div>
+				        <br>
 
-		           			foreach ($results as $classe) 
-		           			{
-						?>
+				        <div class="eleve">
+							<label>Elève :</label>
+							<div class="nomEleve"> 
+						        <select name="eleve" id="eleve"> 
+						            <?php 
+						           		$query = $bdd->prepare('SELECT Nom_etudiant, Prenom_etudiant, Id_etudiant FROM etudiant');
+						           		$query->execute();
+										$results = $query->fetchAll();
+						           		foreach ($results as $eleve) 
+						           		{
+						           	?>
 
-        					<option value="<?php echo $classe['Id_classe']; ?>">
-    							<?php echo $classe['Nom_classe']; ?>
-        					</option>
-    					<?php
-							}
-						?>
-			        </select>
+						           		<option value="<?php echo $eleve['Id_etudiant']; ?>">
+											<?php echo $eleve['Prenom_etudiant']." ".$eleve['Nom_etudiant']; ?>
+										</option>
+									<?php
+										}
+									?>
+								</select>
+							</div>
+						</div>
 
-			        <br>
-
-					<label>Elève :</label> 
-			        <select name="eleve" id="eleve">
-			            
-			            <?php 
-			           		$query = $bdd->prepare('SELECT Nom_etudiant, Prenom_etudiant, Id_etudiant FROM etudiant');
-			           		$query->execute();
-							$results = $query->fetchAll();
-			           		foreach ($results as $eleve) 
-			           		{
-			           	?>
-
-			           		<option value="<?php echo $eleve['Id_etudiant']; ?>">
-								<?php echo $eleve['Prenom_etudiant']." ".$eleve['Nom_etudiant']; ?>
-							</option>
-						<?php
-							}
-						?>
-					</select>
-				<input type="submit" name="rechercheEleve" value="Rechercher">
-				</form>
+						<div class="recherche">
+							<input type="submit" name="rechercheEleve" value="Rechercher">
+						</div>
+					</form>
+				</div>
+			</div>
+			<?php 
+			// On inclue le footer à la page
+			include 'view/includes/footer.php';
+			?>
+		</div>
+</div>

d3778b4bad881f4c84985d44253b26585e580d47
Dylan Noally <Dylan Noally>
2017-04-29 16:35:21 +0200

    Ajout Année dans la BD

--- nouvelleAnnee.php
+++ nouvelleAnnee.php
@@ -10,13 +10,12 @@
 			<div class="left">
 				<h2> // CLASSE(BD)  LISTE ELEVE DE LA CLASSE// </h2>
 					<br>
+					<br>
 					
 					<h2>Historique des élèves par année</h2>
-
-					<br>
 					<br>
 
-					<form action="classe.php" method="GET" >
+					<form action="nouvelleAnnee.php" method="POST" >
 						<label>Année :</label>
 				           <select name="annee" id="annee">
 				           	<?php 
@@ -36,8 +35,62 @@
 							?>
 				           </select>
 
-					ou nouvelle :<input type="text" name="nouvelleAnnee">
+					ou nouvelle :<input type="text" maxlength="9" name="nouvelleAnnee" format="NNNNN">
 					<br>
 
-					<input type="submit" name="historiqueEleveValider" value="Validers">
+					<input type="submit" name="historiqueEleveValider" value="Valider">
 					</form>
+
+					<?php
+						// Vérification de la connexion à la base de données
+						// Si nous sommes bien connecté
+						if ($bdd == true) 
+						{
+							// Préparation de la requête SQL suivante : inserer la classe
+							$query = $bdd->prepare("INSERT INTO annee (Annee) values (:ajoutAnnee)");
+							$query->bindParam(':ajoutAnnee' , $ajoutAnnee);
+							// Vérification de l'existence des valeurs du formulaire
+							if (isset($_POST["historiqueEleveValider"]) && !empty($_POST["nouvelleAnnee"])) 
+							{
+								// Récuperation des valeurs du formulaire
+								$ajoutAnnee = $_POST["nouvelleAnnee"];
+
+								// On extrait toutes les années présentes dans la table Année, puis on compare cette dernière
+								// avec la valeur transmise dans le formulaire pour vérifier si cela ne créera pas un doublon.
+								$query2 = $bdd->prepare('SELECT Annee FROM annee');
+					           	$query2->execute();
+					           	$results = $query2->fetchAll();
+
+					           	var_dump($results);
+
+					           	// On recherche dans le resultat de la requête sous forme de tableau si il y a un doublon entre ce 
+					           	// que l'utilisateur à saisie et ce qu'il y a dans la Base de Données
+					           	foreach ($results as $uneAnnee) 
+					           	{
+					           		if($results == $ajoutAnnee)
+					           		{
+										// header('Location: nouvelleAnnee.php');
+					           			echo "L\n 'année que vous souhaitez ajouter est déjà enregistrée !";
+					           		}
+					           		else
+					           		{
+					           			// Exécution de la requête 
+										$query->execute();
+
+										// Arrêt forcé du Foreach pour éviter qu'il ne se rééxectue une fois de plus
+										break;
+
+					           		}									
+					           	}
+							}
+							//else
+							//{
+
+							//	echo 'RIEN';
+							//}		
+						}
+						elseif ($bdd == false) 
+						{
+							echo "Echec de connexion à la base de données";
+						}
+					?>

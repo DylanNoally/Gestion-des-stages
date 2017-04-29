bd2b8f43caab694074995ca120261d9a6ccb41fa
Dylan Noally <Dylan Noally>
2017-04-29 20:48:11 +0200

    Correctifs Ajout Stage

--- traitementAjoutStage.php
+++ traitementAjoutStage.php
@@ -1,14 +1,61 @@
 <?php
-	setlocale(LC_TIME, 'fra_fra');
 
-	if(isset($_POST["creerStage"]))
-	{
+		setlocale(LC_TIME, 'fra_fra');
 
-			$dateDeb = DateTime::createFromFormat('d/m/Y', $_POST['dateDebut']);
-			$dateFin = DateTime::createFromFormat('d/m/Y', $_POST['dateFin']);
+		// Vérification de la connexion à la base de données
+		// Si nous sommes bien connecté
+		if ($bdd == true) 
+		{
+			// Préparation de la requête SQL suivante : inserer le stage
+			$query = $bdd->prepare("INSERT INTO stage (Date_debut_stage, Date_fin_stage, Type_stage, Observations_stage, Id_etudiant, Id_referent_peda, Id_entreprise, Id_referent_pro, Id_date_annee) values (:dateDebut, :dateFin, 'Développement Application', 'Etudiant sérieux et une assiduité omniprésente', :idEleve, 1, 1, 1, :idDateAnnee)");
 
-			$requetInsertProgram = $bdd->prepare('INSERT INTO stage(Id_stage, Date_debut_stage, Date_fin_stage, Type_stage, Observations_stage, Id_etudiant, Id_referent_peda, Id_entreprise, Id_referent_pro, Id_date_annee) VALUES (4, :dateDebut, :dateFin, "Développement Application", "Etudiant sérieux et une assiduité omniprésente"), 4, 1, 1, 1, 1'); 
-	 
-			$requetInsertProgram->execute();
+			$query->bindParam(':dateDebut' , $dateDebut);
+			$query->bindParam(':dateFin' , $dateFin);
+			$query->bindParam(':idEleve' , $_SESSION['idEleve']);
+			$query->bindParam(':idDateAnnee' , $_POST['annee']);
 
-	}
+			// Vérification de l'existence des valeurs du formulaire
+			if (isset($_POST["creerStage"])) 
+			{
+				// Récuperation des valeurs du formulaire
+				$dateDeb = DateTime::createFromFormat('d/m/Y', $_POST['dateDebut']);
+				$dateFin = DateTime::createFromFormat('d/m/Y', $_POST['dateFin']);
+
+				// On extrait toutes les années présentes dans la table Année, puis on compare cette dernière
+				// avec la valeur transmise dans le formulaire pour vérifier si cela ne créera pas un doublon.
+				// $query2 = $bdd->prepare('SELECT Annee FROM annee');
+				// $query2->execute();
+				// $results = $query2->fetchAll();
+
+
+				// On recherche dans le resultat de la requête sous forme de tableau si il y a un doublon entre ce 
+				// que l'utilisateur à saisie et ce qu'il y a dans la Base de Données
+				//foreach ($results as $uneAnnee) 
+					//{
+					    //if($results == $ajoutAnnee)
+					    //{
+							// header('Location: nouvelleAnnee.php');
+					        // echo "L\n 'année que vous souhaitez ajouter est déjà enregistrée !";
+					    //}
+					    //else
+					    //{
+					        //Exécution de la requête 
+							$query->execute();
+
+							// Arrêt forcé du Foreach pour éviter qu'il ne se rééxectue une fois de plus
+							// break;
+
+					    //}									
+					//}
+			}
+			//else
+			//{
+
+			//	echo 'RIEN';
+			//}		
+		}
+		elseif ($bdd == false) 
+		{
+			echo "Echec de connexion à la base de données";
+		}
+?>

7e4342f99a280f8e31015d228eb6e8d17ba42a07
DylanP60 <popyk.dylan@9online.fr>
2017-05-03 19:45:13 +0200

    Add files via upload

--- nouvelleClasse.php
+++ nouvelleClasse.php
@@ -2,14 +2,14 @@
 		session_start();
 		include 'view/includes/header.php';
 		include 'view/includes/connexionBD.php';
-		include 'view/includes/traitementNouvelleClasse.php';
+		
 	?>
 
 		<h1>Suivi Scolarité</h1>
 		<br>
 		<h3>Ajouter une nouvelle classe</h3>
 		<br>
-		<form method="post" action="stage.php">
+		<form method="post" action="classe.php">
 		Libellé : <input type="text" class="text1" name="LibelléClasse">
 		<br><br>
 		<div class='bouton213'>
@@ -17,3 +17,4 @@
 		</form>
 		<input type="reset" name="BoutonReset" value="Réinitialiser" onclick="nouvelleClasse.php">
 		</div>
+ 

6bfcbe94672127b3921211eb3db4077675df0768
GradiM <gradi.mandambu@outlook.com>
2017-05-04 02:54:31 +0200

    Add files via upload

--- view/includes/traitementAjouRefPro.php
+++ view/includes/traitementAjouRefPro.php
@@ -2,11 +2,6 @@
 	// Vérification de la connexion à la base de données
 	// Si nous sommes bien connecté
 	if ($bdd == true) {
-		// Préparation de la requête SQL suivante : inserer le nom et la fonction du référent professionel
-		$query = $bdd->prepare("INSERT INTO referent_pro (Nom_referent_pro , Fonction_referent_pro) values (:nomRef , :fonctionRef)");
-		$query->bindParam(':nomRef' , $nomRef);
-		$query->bindParam(':fonctionRef' , $fonctionRef);
-
 		// Vérification de l'existence des valeurs du formulaire
 		if (isset($_POST["nom"]) && isset($_POST["fonction"])) {
 			if(empty($_POST["nom"])) {
@@ -14,21 +9,20 @@
 					<script type="text/javascript"> alert("Veuillez inscrire le nom du référent"); </script> 
 					<?php
 				}
+			else {
+				// Récuperation des valeurs du formulaire
+				$nomRef = $_POST["nom"];
+				$nomRef = strtoupper($nomRef); // Tout mettre en majuscule
+				$fonctionRef = $_POST["fonction"];
 
-				elseif ($_POST["nom"] != 'string') {
-					?>
-					<script type="text/javascript"> alert("Un nom ne possède pas de chiffres ou de caractères spéciaux !"); </script> 
-					<?php
-				}
-		
-				else {
-					// Récuperation des valeurs du formulaire
-					$nomRef = $_POST["nom"];
-					$fonctionRef = $_POST["fonction"];
+				// Préparation de la requête SQL suivante : inserer le nom et la fonction du référent professionel
+				$query = $bdd->prepare("INSERT INTO referent_pro (Nom_referent_pro , Fonction_referent_pro) values (:nomRef , :fonctionRef)");
+				$query->bindParam(':nomRef' , $nomRef);
+				$query->bindParam(':fonctionRef' , $fonctionRef);
 
-					// Exécution de la requête 
-					$query->execute();
-				}
+				// Exécution de la requête 
+				$query->execute();
+			}
 		}
 	}
 					
--- view/includes/traitementClassePageClasse.php
+++ view/includes/traitementClassePageClasse.php
@@ -1,12 +1,23 @@
-<?php if (isset($_GET['la_classe'])) {
+<?php 
+		if (isset($_GET['la_classe']) == false) {
+		?>
+			<script type="text/javascript"> alert("Erreur : le(s) paramètre(s) situé(s) dans l'URL ne sont pas ou ne sont plus présent(s)"); </script> 
+		<?php
+		}
+
+		elseif (isset($_GET['la_classe']) == true) {
 					
-		if (empty($_GET['la_classe'])) {
-			// On execute rien	
+			if (empty($_GET['la_classe'])) {
+			?>
+				<script type="text/javascript"> alert("Erreur : le(s) paramètre(s) situés dans l'URL n'ont plus de valeur(s)"); </script> 
+				<?php	
+			}
+			else { 
+				$classe = $_GET['la_classe'];
+			}
 		}
-		else { 
-			$classe = $_GET['la_classe'];
-		}
-	}
+
+
 	$extraireClasse = $bdd->prepare('SELECT Nom_classe FROM classe WHERE Nom_classe = ?');
 		// Exécution de la requête 
 	$extraireClasse->execute(array($classe));
